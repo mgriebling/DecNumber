c66873de8d0ad4321c0277f2c5a2b0f13b29bb36
Michael Griebling <sam@c-inspirations.com>
2018-02-24 11:47:39 -0500

    Initial Commit

---
+++ DecNumber_iOS/DecNumber_iOS.h
@@ -0,0 +1,19 @@
+//
+//  DecNumber_iOS.h
+//  DecNumber_iOS
+//
+//  Created by Mike Griebling on 24 Feb 2018.
+//  Copyright © 2018 Computer Inspirations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DecNumber_iOS.
+FOUNDATION_EXPORT double DecNumber_iOSVersionNumber;
+
+//! Project version string for DecNumber_iOS.
+FOUNDATION_EXPORT const unsigned char DecNumber_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DecNumber_iOS/PublicHeader.h>
+
+
---
+++ DecNumber_iOSTests/DecNumber_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  DecNumber_iOSTests.swift
+//  DecNumber_iOSTests
+//
+//  Created by Mike Griebling on 24 Feb 2018.
+//  Copyright © 2018 Computer Inspirations. All rights reserved.
+//
+
+import XCTest
+@testable import DecNumber_iOS
+
+class DecNumber_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
